83ca8202074fe5e0852dc0fba77cede69ff9afa0
sajib-rana <sajibrana444@gmail.com>
2023-10-08 23:12:24 +0600

    user save in database

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.ynhrrkd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynhrrkd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,21 @@ async function run() {
     await client.connect();
 
      const usersCollection = client.db("JerinParlour").collection("users");
+
+
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await usersCollection.findOne(query);
+
+        if (existingUser) {
+          return res.send({ message: "user already exists" });
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+      
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
